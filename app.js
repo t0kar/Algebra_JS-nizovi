a05083bbe67188b2c7029695a06d585dd4adce02
t0kar <81991067+t0kar@users.noreply.github.com>
2021-06-17 19:01:12 +0200

    .pop() replaced with continue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var fizzBuzz = [];
 
 for (var i = 0; i <= 20; i++) {
-    if (i === 0) fizzBuzz.pop();
+    if (i === 0) continue;
     else if (i % 15 === 0) fizzBuzz.push("fizzbuzz");
     else if (i % 3 === 0) fizzBuzz.push("fizz");
     else if (i % 5 === 0) fizzBuzz.push("buzz");
